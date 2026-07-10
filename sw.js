283fcb64f1c85705e6cc0a975722b5a012afb526
abiyhawaz3-beep <abiyhawaz3@gmail.com>
2026-07-10 20:23:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'rrace-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try fresh, fall back to cache if offline
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(res=>{
+        const clone = res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, clone));
+        return res;
+      })
+      .catch(()=> caches.match(e.request))
+  );
+});
